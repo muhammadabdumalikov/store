0a829e447dc4084c0df1c4549550c896100378b7
muhammadabdumalikov <burakali424@gmail.com>
2023-09-07 13:52:24 +0500

    first

---
+++ src/providers/base-dao.ts
@@ -0,0 +1,59 @@
+import { Knex } from 'knex';
+import { KnexService } from './knex.service';
+import ObjectID from 'bson-objectid';
+
+// export interface IBaseQuery<T> {
+//   selectById(id: string, columns?: string[]): Knex.QueryBuilder<T>;
+// }
+
+export class KnexBaseRepo {
+  knexService: KnexService;
+
+  constructor() {
+    this.knexService = new KnexService();
+  }
+
+  get knex(): Knex {
+    return this.knexService.instance;
+  }
+
+  generateRecordId() {
+    return new ObjectID().toString();
+  }
+}
+
+export class BaseRepo<T extends {}> extends KnexBaseRepo {
+  _tableName: string;
+  constructor(tableName: string) {
+    super();
+    this._tableName = tableName;
+  }
+
+  get getTableName() {
+    return this._tableName;
+  }
+
+  selectById(id: string, columns = ['*']): Knex.QueryBuilder<T> {
+    return this.knex
+      .select(columns)
+      .from(this._tableName)
+      .where('id', id)
+      .first();
+  }
+
+  _insert(values, options) {
+    const { returning = ['*'], generateId = true } = options;
+    if (Array.isArray(values) && values.length > 0 && generateId) {
+      values.forEach((value) => {
+        value.id = this.generateRecordId();
+      });
+    }
+    if (!Array.isArray(values) && generateId && !values.id) {
+      values.id = this.generateRecordId();
+    }
+    if (generateId && values && Array.isArray(values)) {
+      values = values.map((v) => ({ ...v, id: this.generateRecordId() }));
+    }
+    return this.knex.insert(values).into(this._tableName).returning(returning);
+  }
+}
---
+++ src/providers/knex.service.ts
@@ -0,0 +1,17 @@
+import knex, { Knex } from "knex";
+
+export class KnexService {
+  instance: Knex;
+
+  constructor() {
+    this.instance = knex({
+      client: 'postgresql',
+      connection: {
+        host: 'rain.db.elephantsql.com',
+        database: 'jsskmqpe',
+        password: 'IIWPSMjS-Y4bxIR5F7MqRUhDm0eAnVxq',
+        user: 'postgres'
+      }
+    })
+  }
+}
