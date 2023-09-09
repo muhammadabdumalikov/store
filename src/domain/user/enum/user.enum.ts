a8078eaa30e152033a37e9bbe8b40b51727fd270
muhammadabdumalikov <burakali424@gmail.com>
2023-09-09 15:54:44 +0500

    add otp check

--- src/domain/user/enum/user.enum.ts
+++ src/domain/user/enum/user.enum.ts
@@ -2,3 +2,8 @@ export enum UserRoles {
   SELLER = 1,
   ADMIN = 2,
 }
+
+export enum UserStatus {
+  REGISTERED = 0,
+  ACTIVE = 1,
+}
--- src/domain/user/user.service.ts
+++ src/domain/user/user.service.ts
@@ -1,24 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto, UpdateUserDto } from './dto/user.dto';
 import { UserRepo } from './user.repo';
-import { UserRoles } from './enum/user.enum';
+import { UserRoles, UserStatus } from './enum/user.enum';
+import { IUser } from './interface/user.interface';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly userRepo: UserRepo) { }
+  constructor(private readonly userRepo: UserRepo) {}
 
   async signUp(params: CreateUserDto) {
     const otp = Math.floor(10000 + Math.random() * 90000);
 
-    const query = this.userRepo.insert({
+    const [user]: [IUser] = await this.userRepo.insert({
       phone: params.phone,
       // first_name: params.first_name,
       // last_name: params.last_name,
       role: UserRoles.SELLER,
       otp: otp,
+      status: UserStatus.REGISTERED,
     });
 
-    return query;
+    return { otp: user.otp, phone: user.phone };
   }
 
   findAll() {
