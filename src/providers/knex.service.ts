87298f01ae2d94b219cf6958e692b4c8236c097c
Muhammad Abdumalik <burakali424@gmail.com>
2023-10-05 21:05:24 +0500

    Update knex.service.ts

--- src/providers/knex.service.ts
+++ src/providers/knex.service.ts
@@ -7,14 +7,14 @@ export class KnexService {
     this.instance = knex({
       client: 'postgresql',
       connection: {
-        host: 'rain.db.elephantsql.com',
-        database: 'jsskmqpe',
-        password: 'IIWPSMjS-Y4bxIR5F7MqRUhDm0eAnVxq',
-        user: 'jsskmqpe',
+        host: 'localhost',
+        database: 'store',
+        password: '5432',
+        user: 'postgres'
       },
       pool: {
-        min: 1,
-        max: 4,
+        min: 2,
+        max: 75,
       },
     })
   }
